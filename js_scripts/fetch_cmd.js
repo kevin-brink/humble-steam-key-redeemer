63660bff06f57e5163d50883e73fe4ce4717c278
kevin-brink <kbrink170@gmail.com>
2025-11-02 09:39:31 -0600

    Use url-encoding instead of form data

--- js_scripts/fetch_cmd.js
+++ js_scripts/fetch_cmd.js
@@ -3,16 +3,17 @@ var jsonData = arguments[0];
 var url = arguments[1];
 var csrf = arguments[2];
 
-var formData = new FormData();
+var params = new URLSearchParams();
 for (const key in jsonData) {
-  formData.append(key, jsonData[key]);
+  params.append(key, jsonData[key]);
 }
 
 fetch(url, {
   headers: {
-    "csrf-prevention-token": csrf
+    "csrf-prevention-token": csrf,
+    "Content-Type": "application/x-www-form-urlencoded"
   },
-  body: formData,
+  body: params.toString(),
   method: "POST",
 })
   .then(r => {
